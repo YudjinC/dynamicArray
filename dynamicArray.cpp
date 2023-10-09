4cb9d6901b664c0ea943b1204bb0b4cb051f5f86
UjinA <mya4ikoff.eugeny@yandex.ru>
2023-10-10 02:24:37 +0300

    fix indexes witch forrward(), + free()

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -7,8 +7,10 @@ void inputArr(int *arr, int sizeArr);
 void outputArr(int *arr, int sizeArr);
 void pushForward(int *&arr, int &sezeArr, int pushValue);
 void pushBack(int *&arr, int &sizeArr, int pushValue);
+void pushFree(int*& arr, int& sizeArr, int pushValue, int position);
 void deleteForward(int *&arr, int &sizeArr);
 void deleteBack(int *&arr, int &sizeArr);
+void deleteFree(int *&arr, int &sizeArr, int position);
 
 int main() {
 	srand(time(NULL));
@@ -46,6 +48,16 @@ int main() {
 	outputArr(arr, sizeArr);
 	cout << endl;
 
+	cout << "Push free value:" << endl;
+	pushFree(arr, sizeArr, rand() % 90, rand() % (sizeArr + 1));
+	outputArr(arr, sizeArr);
+	cout << endl;
+
+	cout << "Delete free value:" << endl;
+	deleteFree(arr, sizeArr, rand() % sizeArr);
+	outputArr(arr, sizeArr);
+	cout << endl;
+
 	delete[] arr;
 	arr = nullptr;
 }
@@ -67,8 +79,8 @@ void pushForward(int *&arr, int &sizeArr, int pushValue) {
 	int *newArr = new int[++sizeArr];
 	newArr[0] = pushValue;
 	
-	for (int i = 0, j = 1; i < sizeArr - 1; i++, j++) {
-		newArr[j] = arr[i];
+	for (int i = 0; i < sizeArr - 1; i++) {
+		newArr[i + 1] = arr[i];
 	}
 
 	arr = newArr;
@@ -85,19 +97,48 @@ void pushBack(int *&arr, int &sizeArr, int pushValue) {
 	arr = newArr;
 }
 
+void pushFree(int*& arr, int& sizeArr, int pushValue, int position) {
+	position > sizeArr ? position-- : NULL;
+	int *newArr = new int[++sizeArr];
+	for (int i = 0; i < position; i++) {
+		newArr[i] = arr[i];
+	}
+
+	newArr[position] = pushValue;
+
+	for (int i = position; i < sizeArr - 1; i++) {
+		newArr[i + 1] = arr[i];
+	}
+
+	arr = newArr;
+}
+
 void deleteForward(int*& arr, int& sizeArr) {
 	int *newArr = new int[--sizeArr];
-	for (int i = 1, j = 0; i < sizeArr + 1; i++, j++) {
-		newArr[j] = arr[i];
+	for (int i = 1; i < sizeArr + 1; i++) {
+		newArr[i - 1] = arr[i];
 	}
 
 	arr = newArr;
 }
 
 void deleteBack(int *&arr, int &sizeArr) {
-	int* newArr = new int[--sizeArr];
+	int *newArr = new int[--sizeArr];
 	for (int i = 0; i < sizeArr; i++) {
 		newArr[i] = arr[i];
+	}
+
+	arr = newArr;
+}
+
+void deleteFree(int*& arr, int& sizeArr, int position) {
+	position == sizeArr ? position-- : NULL;
+	int *newArr = new int [--sizeArr];
+	for (int i = 0; i < position; i++) {
+		newArr[i] = arr[i];
+	}
+	for (int i = position + 1; i < sizeArr + 1; i++) {
+		newArr[i - 1] = arr[i];
 	}
 
 	arr = newArr;
